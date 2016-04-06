99ac3947abd6b9d9beb27285f9d8f408652f435b
Troy Partridge <troy.partridge@sendgrid.com>
2016-04-06 11:40:51 -0700

    Update fortress init

--- fortress/init.sh
+++ fortress/init.sh
@@ -8,17 +8,19 @@ cp $FORTRESS/.bashrc $HOME/.bashrc
 # .bash_profile
 declare -a arr=(
   "# User Defined" 
-  "export PATH=$PATH:/home/vagrant/bin:/usr/bin"
+  "export PATH=\$PATH:/home/vagrant/bin:/usr/bin"
   "export DISPLAY=:99"
   "if [ -f ~/.bashrc ]; then"
   "   source ~/.bashrc"
   "fi"
-  "test -f ~/.git-completion.bash && . $_"
-  "cd $HOME/fortress_development/development_projects"
+  "test -f ~/.git-completion.bash && . \$_"
+  "cd \$HOME/fortress_development/development_projects"
   "alias ll='ls -al -G --color'"
   "alias ls='ls -G --color'"
   "alias activate='source venv/bin/activate'"
   "alias pp='python -mjson.tool'"
+  "alias gitcleanup='git branch --merged | grep -v \"\*\" | xargs -n 1 git branch -d'"
+  "alias gitcleanupf='git branch | grep -v \"\*\" | xargs -n 1 git branch -D'"
 )
 
 for i in "${arr[@]}"
@@ -34,4 +36,5 @@ cp $FORTRESS/.gitconfig $HOME/.gitconfig
 if [ ! -d "$HOME/bin" ]; then
   mkdir $HOME/bin
 fi
-cp $CURRENT/scripts/gra $HOME/bin
+cp $CURRENT/scripts/* $HOME/bin
+chmod 755 -R $HOME/bin
