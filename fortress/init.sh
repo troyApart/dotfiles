c80cfc9c90527447a027065e93e5136d71494aa2
Troy Partridge <troy.partridge@sendgrid.com>
2016-04-05 17:30:42 -0700

    Initial setup for fortress vagrant

---
+++ fortress/init.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+CURRENT=$PWD
+FORTRESS=$CURRENT/fortress
+
+# .bashrc
+cp $FORTRESS/.bashrc $HOME/.bashrc
+
+# .bash_profile
+declare -a arr=(
+  "# User Defined" 
+  "export PATH=$PATH:/home/vagrant/bin:/usr/bin"
+  "export DISPLAY=:99"
+  "if [ -f ~/.bashrc ]; then"
+  "   source ~/.bashrc"
+  "fi"
+  "test -f ~/.git-completion.bash && . $_"
+  "cd $HOME/fortress_development/development_projects"
+  "alias ll='ls -al -G --color'"
+  "alias ls='ls -G --color'"
+  "alias activate='source venv/bin/activate'"
+  "alias pp='python -mjson.tool'"
+)
+
+for i in "${arr[@]}"
+do
+   echo "$i" >> $HOME/.bash_profile
+done
+
+# Git
+cp $FORTRESS/.git-completion.bash $HOME/.git-completion.bash
+cp $FORTRESS/.gitconfig $HOME/.gitconfig
+
+# Scripts
+if [ ! -d "$HOME/bin" ]; then
+  mkdir $HOME/bin
+fi
+cp $CURRENT/scripts/gra $HOME/bin
